3fb40e7617957ca1f539f53991ba4414d3c1ba1b
imtiaze_techvill <imtiaze.techvill@gmail.com>
2020-03-07 00:58:58 +0600

    Item made dynamic

--- index.php
+++ index.php
@@ -3,6 +3,100 @@
 	$connect = new PDO("mysql:host=localhost;dbname=weblessions_shopping_cart_php_mysql_cookies", "root", "");
 
 
+
+	if (isset($_POST["add_to_cart"])) 
+	{
+		if (isset($_COOKIE["shopping_cart"]))
+		{
+			$cookie_data = stripcslashes($_COOKIE["shopping_cart"]);
+			$cart_data   = json_decode($cookie_data, true);
+		}
+		else
+		{
+			$item_array = array();
+		}
+
+		$item_id_list = array_column($cart_data, 'item_id');
+
+		if (in_array($_POST['hidden_id'], $item_id_list)) 
+		{
+			foreach ($cart_data as $keys => $values) {
+				if ($cart_data[$keys]['item_id'] ==  $_POST["hidden_id"]) 
+				{
+					$cart_data[$keys]['item_quantity'] = $cart_data[$keys]["item_quantity"] + ($_POST["quantity"] <= '0' ? '1' : $_POST["quantity"]);
+				}
+			}
+		}
+		else 
+		{
+			$item_array = array(
+				'item_id' 		=> $_POST["hidden_id"],
+				'item_name' 	=> $_POST["hidden_name"],
+				'item_price' 	=> $_POST["hidden_price"],
+				'item_quantity' => $_POST["quantity"] <= '0' ? '1' : $_POST["quantity"],
+			);
+	
+			$cart_data[] = $item_array;
+		}
+
+
+		
+		$item_data = json_encode($cart_data);
+		
+		setcookie('shopping_cart', $item_data, time() + (86400 * 30));
+
+		header("location:index.php?success=1");
+	}
+
+	if (isset($_GET["action"]))
+	{
+		if (isset($_GET["action"]) == 'delete') 
+		{
+			$cookie_data = stripslashes($_COOKIE['shopping_cart']);
+			$cart_data	 = json_decode($cookie_data, true);
+
+			foreach($cart_data as $keys => $values)
+			{
+				if ($cart_data[$keys]['item_id'] == $_GET["id"])
+				{
+					unset($cart_data[$keys]);
+					$item_data = json_encode($cart_data);
+					setcookie("shopping_cart", $item_data, time() + (86400 * 30));
+					header("location:index.php?remove=1");
+				}
+			}
+		}
+	}
+
+	if (isset($_GET['success']))
+	{
+		$message = '
+			<div class="alert alert-success alert-dismissiable">
+				<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+				Item Added into Cart
+			</div>
+		';
+	}
+
+	if (isset($_GET['remove']))
+	{
+		$message = '
+			<div class="alert alert-danger alert-dismissiable">
+				<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+				Item Remove from Cart
+			</div>
+		';
+	}
+
+	if (isset($_GET['action']))
+	{
+		if ($_GET['action'] == 'clear')
+		{
+			setcookie("shopping_cart", "", time() - 3600);
+			header("location:index.php");
+		}
+	}
+
 ?>
 
 <!doctype html>
@@ -59,6 +153,9 @@
 		<div style="clear:both"></div>
 		<br />
 		<h3>Order Details</h3>
+
+		<?php if (isset($message)) echo $message;  ?>
+
 		<div class="table-responsive">
 			<div align="right">
 				<a href="index.php?action=clear"><b>Clear Cart</b></a>
@@ -71,25 +168,53 @@
 					<th width="15%">Total</th>
 					<th width="5%">Action</th>
 				</tr>
-				<tr>
-					<td>test</td>
-					<td>test</td>
-					<td>test</td>
-					<td>test</td>
-					<td>
-						<a href=""><span class="text-danger">Remove</span></a>
-					</td>
-				</tr>
-   
-				<tr>
-					<td colspan="3" align="right">Total</td>
-					<td align="right">test</td>
-					<td></td>
-				</tr>
- 
-				<tr>
-					<td colspan="5" align="center">No Item in Cart</td>
-				</tr>
+
+				<?php 
+
+					if (isset($_COOKIE["shopping_cart"]))
+					{
+						$total = 0;
+
+						$cookie_data = stripcslashes($_COOKIE["shopping_cart"]);
+
+						$cart_data = json_decode($cookie_data, true);
+
+						foreach ($cart_data as $keys => $values) 
+						{
+							?>
+								<tr>
+									<td><?php echo $values["item_name"]; ?></td>
+									<td><?php echo $values["item_quantity"]; ?></td>
+									<td><?php echo $values["item_price"]; ?></td>
+									<td><?php echo ($values["item_price"] * number_format($values["item_quantity"])); ?></td>
+									<td>
+										<a href="index.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a>
+									</td>
+								</tr>	
+							<?php
+
+								$total =  $total + ($values["item_price"] * number_format($values["item_quantity"]));
+						}
+
+						  	?>
+								<tr>
+									<td colspan="3" align="right">Total</td>
+									<td align="right"><?php echo $total; ?></td>
+									<td></td>
+								</tr>
+							<?php				
+					}
+					else
+					{
+						echo '
+							<tr>
+								<td colspan="5" align="center">No Item in Cart</td>
+							</tr>
+						';
+					}
+
+				?>
+
 			</table>
 		</div>
 	</div>
